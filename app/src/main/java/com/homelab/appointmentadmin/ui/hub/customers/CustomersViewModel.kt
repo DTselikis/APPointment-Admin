5b666a63815aa44b37544b4728f8005c2525d161
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-16 10:50:16 +0300

    Removed users pagination

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/CustomersViewModel.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/CustomersViewModel.kt
@@ -13,19 +13,13 @@ class CustomersViewModel : ViewModel() {
     private val _users = MutableLiveData<List<User>>()
     val users: LiveData<List<User>> = _users
 
-    private var page = 0
-    private val itemsPerPage = 15L
-
     fun fetchUsersFromDB() {
         Firebase.firestore.collection(USERS_COLLECTION)
-            .startAt(page * itemsPerPage)
-            .limit(itemsPerPage)
             .get()
             .addOnSuccessListener { result ->
                 _users.value = result.map { document ->
                     document.toObject<User>()
                 }
-                page++
             }
     }
 }

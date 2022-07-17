e3f796aa7343b4b0feebedf8572766a56d15dc0d
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-17 15:14:09 +0300

    Fixed a bug where snackbar was not shown

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/edit/CustomerProfileEditFragment.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/edit/CustomerProfileEditFragment.kt
@@ -88,6 +88,7 @@ class CustomerProfileEditFragment : Fragment() {
                         closeEditsFragment()
                     }
                 })
+                .show()
         }
     }
 

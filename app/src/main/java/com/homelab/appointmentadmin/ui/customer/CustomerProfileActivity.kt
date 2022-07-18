bfa8338ddd06f8f62e6c6fe22c16b4de1348d016
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-18 13:40:01 +0300

    Semi working card animation

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/CustomerProfileActivity.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/CustomerProfileActivity.kt
@@ -2,15 +2,25 @@ package com.homelab.appointmentadmin.ui.customer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.viewModels
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.navArgs
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.card.MaterialCardView
 import com.homelab.appointmentadmin.R
 import com.homelab.appointmentadmin.databinding.ActivityCustomerProfileBinding
+import kotlin.math.max
 
 class CustomerProfileActivity : AppCompatActivity() {
 
@@ -49,5 +59,105 @@ class CustomerProfileActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.profile_nav_host) as NavHostFragment
         val navController = navHostFragment.navController
         navController.navigate(R.id.action_profile_contact_item_to_customerProfileEditFragment)
+
+        minimizeCardForEdit()
+    }
+
+    fun exitProfileEdit() {
+        onBackPressed()
+        maximizeCard()
+    }
+
+    private fun maximizeCard() {
+        TransitionManager.beginDelayedTransition(
+            binding.photoCard as ViewGroup,
+            AutoTransition()
+        )
+        binding.apply {
+            photoCard.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    270f,
+                    resources.displayMetrics
+                ).toInt()
+            }
+
+            customerProfilePic.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    120f,
+                    resources.displayMetrics
+                ).toInt()
+                width = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    120f,
+                    resources.displayMetrics
+                ).toInt()
+
+                horizontalBias = 0.5f
+            }
+
+            customerNickname.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                topToBottom = customerProfilePic.id
+                startToStart = customerProfilePic.id
+                endToEnd = customerProfilePic.id
+                bottomToBottom = photoCard.id
+
+                horizontalBias = 0.5f
+                verticalBias = 0.2f
+            }
+
+            navView.visibility = View.VISIBLE
+            cardEditBtnIcon.visibility = View.VISIBLE
+            cardBackBtnIcon.visibility = View.GONE
+            cardSaveBtnIcon.visibility = View.GONE
+        }
+
+    }
+
+    private fun minimizeCardForEdit() {
+        TransitionManager.beginDelayedTransition(
+            binding.photoCard as ViewGroup,
+            AutoTransition()
+        )
+        binding.apply {
+            photoCard.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    40f,
+                    resources.displayMetrics
+                ).toInt()
+            }
+
+            customerProfilePic.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                height = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    35f,
+                    resources.displayMetrics
+                ).toInt()
+                width = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    35f,
+                    resources.displayMetrics
+                ).toInt()
+
+                horizontalBias = 0.3f
+            }
+
+            customerNickname.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                topToTop = customerProfilePic.id
+                bottomToBottom = customerProfilePic.id
+                endToStart = cardSaveBtnIcon.id
+                startToEnd = customerProfilePic.id
+
+                horizontalBias = 0.25f
+                verticalBias = 0.5f
+            }
+
+            navView.visibility = View.GONE
+            cardEditBtnIcon.visibility = View.GONE
+            cardBackBtnIcon.visibility = View.VISIBLE
+            cardSaveBtnIcon.visibility = View.VISIBLE
+        }
     }
 }

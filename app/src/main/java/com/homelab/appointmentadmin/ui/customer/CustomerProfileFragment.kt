5e3935691455cf4065b294d14185c12f49bc230e
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-18 21:34:49 +0300

    Setup viewpager

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/CustomerProfileFragment.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/CustomerProfileFragment.kt
@@ -5,20 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
-import androidx.navigation.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
 import com.homelab.appointmentadmin.R
 import com.homelab.appointmentadmin.databinding.FragmentCustomerProfileBinding
 
 class CustomerProfileFragment : Fragment() {
 
-    val args: CustomerProfileFragmentArgs by navArgs<CustomerProfileFragmentArgs>()
-    private val sharedViewModel: CustomerProfileSharedViewModel by activityViewModels()
     private lateinit var binding: FragmentCustomerProfileBinding
 
     override fun onCreateView(
@@ -33,18 +26,11 @@ class CustomerProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupViewPager()
-
-        sharedViewModel.setUser(args.user)
-        binding.apply {
-            lifecycleOwner = viewLifecycleOwner
-            sharedViewModel = this@CustomerProfileFragment.sharedViewModel
-            customerProfileFragment = this@CustomerProfileFragment
-        }
     }
 
     private fun setupViewPager() {
         val pager = binding.viewPager
-        val tabLayout = binding.tableLayout
+        val tabLayout = binding.tabLayout
 
         pager.adapter = CustomerPagerAdapter(this@CustomerProfileFragment)
         TabLayoutMediator(tabLayout, pager) { tab, position ->

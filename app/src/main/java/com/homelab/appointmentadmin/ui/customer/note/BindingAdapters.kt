02d4c5f89244ae22a2eb0d57cfba14cfdd2310c5
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-26 14:41:33 +0300

    Format timestamp

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/note/BindingAdapters.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/note/BindingAdapters.kt
@@ -2,11 +2,21 @@ package com.homelab.appointmentadmin.ui.customer.note
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.Timestamp
 import com.homelab.appointmentadmin.model.network.Note
+import java.text.SimpleDateFormat
 
 @BindingAdapter("notes")
 fun bindNotes(recyclerView: RecyclerView, notes: MutableList<Note>?) {
     val adapter = recyclerView.adapter as NoteAdapter
 
     adapter.submitList(notes)
+}
+
+@BindingAdapter("date")
+fun bindDate(materialTextView: MaterialTextView, timestamp: Timestamp?) {
+    timestamp?.let {
+        materialTextView.text = SimpleDateFormat("E dd/MM/yy").format(timestamp.toDate())
+    }
 }

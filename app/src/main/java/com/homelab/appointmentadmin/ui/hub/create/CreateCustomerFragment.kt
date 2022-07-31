214e5ac79a2609572357a3d01363647ecf8560fe
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-31 16:02:34 +0300

    Show snack bar

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/create/CreateCustomerFragment.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/hub/create/CreateCustomerFragment.kt
@@ -1,5 +1,6 @@
 package com.homelab.appointmentadmin.ui.hub.create
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,7 +9,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.homelab.appointmentadmin.R
+import com.homelab.appointmentadmin.data.NEW_USER_NAV_KEY
+import com.homelab.appointmentadmin.data.USER_NAV_KEY
 import com.homelab.appointmentadmin.databinding.FragmentCreateCustomerBinding
 
 class CreateCustomerFragment : Fragment() {
@@ -33,6 +39,8 @@ class CreateCustomerFragment : Fragment() {
             viewModel = this@CreateCustomerFragment.viewModel
             customerProfileEditFragment = this@CreateCustomerFragment
         }
+
+        observeStoredState()
     }
 
     fun saveCustomer() {
@@ -50,6 +58,26 @@ class CreateCustomerFragment : Fragment() {
             false
         } else {
             true
+        }
+    }
+
+    private fun observeStoredState() {
+        viewModel.userStored.observe(viewLifecycleOwner) { stored ->
+            val text: String
+            val color: Int
+
+            if (stored) {
+                text = getString(R.string.user_creation_success)
+                color = Color.parseColor(getString(R.color.teal_200))
+            } else {
+                text = getString(R.string.user_creation_err)
+                color = Color.parseColor(getString(R.color.email_red))
+            }
+
+            Snackbar.make(binding.saveInfoBtn, text, Snackbar.LENGTH_LONG)
+                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+                .setBackgroundTint(color)
+                .show()
         }
     }
 

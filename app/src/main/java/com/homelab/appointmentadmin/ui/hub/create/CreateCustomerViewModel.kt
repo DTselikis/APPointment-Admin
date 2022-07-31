7d098484b139f533c314cd72d2f8cf2784a88b4c
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-07-31 15:32:04 +0300

    Create User object

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/create/CreateCustomerViewModel.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/hub/create/CreateCustomerViewModel.kt
@@ -2,6 +2,8 @@ package com.homelab.appointmentadmin.ui.hub.create
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.Timestamp
+import com.homelab.appointmentadmin.data.User
 
 class CreateCustomerViewModel : ViewModel() {
     val firstname = MutableLiveData<String>()
@@ -9,4 +11,20 @@ class CreateCustomerViewModel : ViewModel() {
     val nickname = MutableLiveData<String>()
     val phone = MutableLiveData<String>()
     val email = MutableLiveData<String>()
+
+    fun createUser() {
+    }
+
+    private fun getUser(): User =
+        User(
+            uid = null,
+            firstname.value,
+            lastname.value,
+            if (nickname.value.isNullOrBlank()) "${firstname.value} ${lastname.value}" else nickname.value,
+            phone.value,
+            email.value,
+            registered = false
+        ).also {
+            it.uid = "${it.hashCode().toString()}_${Timestamp.now().seconds}"
+        }
 }

fec13bb8a066c16b123eef1a80d1cde714a2bfa3
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-01 19:28:53 +0300

    Display customer profile pic

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/BindingAdapters.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/BindingAdapters.kt
@@ -1,13 +1,18 @@
 package com.homelab.appointmentadmin.ui.hub.customers
 
 import android.util.TypedValue
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.homelab.appointmentadmin.R
 import com.homelab.appointmentadmin.data.User
+import de.hdodenhof.circleimageview.CircleImageView
 
 @BindingAdapter("users")
 fun bindUsersList(recyclerView: RecyclerView, users: List<User>?) {
@@ -28,5 +33,17 @@ fun bindStroke(materialCardView: MaterialCardView, registered: Boolean?) {
             1.1f,
             materialCardView.resources.displayMetrics
         ).toInt()
+    }
+}
+
+@BindingAdapter("imgUrl")
+fun bindProfilePic(circleImageView: CircleImageView, imgUrl: String?) {
+    imgUrl?.let {
+        Firebase.storage.getReferenceFromUrl(imgUrl).downloadUrl.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                circleImageView.load(task.result) {
+                }
+            }
+        }
     }
 }

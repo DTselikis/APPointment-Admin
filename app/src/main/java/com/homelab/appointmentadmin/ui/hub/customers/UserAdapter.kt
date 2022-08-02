527b517767aa9ca0c187d74453bca605721459bd
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-02 12:16:06 +0300

    Add merge option

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/UserAdapter.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/hub/customers/UserAdapter.kt
@@ -53,6 +53,7 @@ class UserAdapter(
             if (!user.registered) {
                 contextMenu.add(view.context.getString(R.string.merge_customers))
                     .setOnMenuItemClickListener {
+                        customersFragment.navigateToMergeUsers(user)
                         true
                     }
             }

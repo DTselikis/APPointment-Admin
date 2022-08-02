301e9632a7207be62f9f3f1463792420b6bd852b
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-02 14:38:14 +0300

    Deleted merge fragment

--- app/src/main/java/com/homelab/appointmentadmin/ui/hub/HubSharedViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.homelab.appointmentadmin.ui.hub
-
-import androidx.lifecycle.ViewModel
-import com.homelab.appointmentadmin.data.User
-
-class HubSharedViewModel : ViewModel() {
-    lateinit var registeredUsers: List<User>
-}

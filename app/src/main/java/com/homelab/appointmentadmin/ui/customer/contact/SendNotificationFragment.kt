542db47e66ff3dde41b14946c1db1b6dd8901500
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-08-30 13:48:42 +0300

    Implemented SendNotificationFragment

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/contact/SendNotificationFragment.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/contact/SendNotificationFragment.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.homelab.appointmentadmin.R
+import com.homelab.appointmentadmin.databinding.FragmentSendNotificationBinding
 
-class SendNotificationFragment : Fragment() {
+class SendNotificationFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentSendNotificationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_notification, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_send_notification, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 }

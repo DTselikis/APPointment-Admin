59873326a24f947336a641e5252a364793f905f8
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-01 13:36:10 +0300

    Created notification model class

---
+++ app/src/main/java/com/homelab/appointmentadmin/model/Notification.kt
@@ -0,0 +1,13 @@
+package com.homelab.appointmentadmin.model
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ServerTimestamp
+
+data class Notification(
+    val title: String? = null,
+    val message: String? = null,
+    val type: Int? = null,
+    val status: Int? = null,
+    @ServerTimestamp
+    val timestamp: Timestamp = Timestamp.now()
+)

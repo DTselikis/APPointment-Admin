704e18f7305669b6a0b6e44b26143870ac9422f6
Dimitris Tselikis <dimitriostselikis@gmail.com>
2022-09-04 14:14:03 +0300

    Added contact providers

--- app/src/main/java/com/homelab/appointmentadmin/ui/customer/contact/CustomerContactFragment.kt
+++ app/src/main/java/com/homelab/appointmentadmin/ui/customer/contact/CustomerContactFragment.kt
@@ -2,8 +2,6 @@ package com.homelab.appointmentadmin.ui.customer.contact
 
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,13 +12,17 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.homelab.appointmentadmin.R
 import com.homelab.appointmentadmin.databinding.FragmentCustomerContactBinding
+import com.homelab.appointmentadmin.model.ContactProviderInfo
 import com.homelab.appointmentadmin.ui.customer.CustomerProfileFragmentDirections
 import com.homelab.appointmentadmin.ui.customer.CustomerProfileSharedViewModel
+import com.homelab.appointmentadmin.utils.ContactProvider
 
 class CustomerContactFragment : Fragment() {
+    private val sharedViewModel: CustomerProfileSharedViewModel by activityViewModels()
 
     private lateinit var binding: FragmentCustomerContactBinding
-    private val sharedViewModel: CustomerProfileSharedViewModel by activityViewModels()
+
+    private lateinit var contactProvidersAdapter: ContactProvidersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,38 +36,104 @@ class CustomerContactFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        contactProvidersAdapter = ContactProvidersAdapter()
+
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
-            sharedViewModel = this@CustomerContactFragment.sharedViewModel
-            fragmentCustomerContact = this@CustomerContactFragment
+            customerContactRv.adapter = contactProvidersAdapter
         }
+
+        contactProvidersAdapter.submitList(constructContactProviders())
     }
 
-    fun callCustomer() {
-        val phone = Uri.parse("tel:${sharedViewModel.user.value!!.phone}")
-        val phoneIntent = Intent(Intent.ACTION_DIAL, phone)
-        phoneIntent.resolveActivity(requireContext().packageManager)?.let {
-            try {
-                startActivity(phoneIntent)
-            } catch (e: ActivityNotFoundException) {
-                showWarningDialog(getString(R.string.dial_app))
+    private fun constructContactProviders(): List<ContactProviderInfo> {
+        val contactProviders = mutableListOf<ContactProviderInfo>()
+        sharedViewModel.user.value?.let { customer ->
+            customer.phone?.let {
+                contactProviders.add(
+                    ContactProviderInfo(
+                        R.color.phone_green,
+                        R.drawable.ic_phone_24,
+                        it
+                    ) {
+                        ContactProvider.callCustomer(requireContext(), it)
+                    }
+                )
             }
 
-        }
-    }
+            customer.fbName?.let {
+                contactProviders.add(
+                    ContactProviderInfo(
+                        R.color.fb_blue,
+                        R.drawable.facebook_logo,
+                        it
+                    ) {
+                        try {
+                            ContactProvider.openFacebookChat(requireContext(), customer.fbProfileId!!)
+                        } catch (e: ActivityNotFoundException) {
+                            showWarningDialog("Facebook")
+                        }
+                    }
+                )
+            }
 
-    fun sendEmail() {
-        val email = Uri.parse("mailto:${sharedViewModel.user.value!!.email}")
-        val emailIntent = Intent(Intent.ACTION_SENDTO, email)
-            .putExtra(Intent.EXTRA_TEXT, getString(R.string.email_signature))
-        emailIntent.resolveActivity(requireContext().packageManager)?.let {
-            try {
-                startActivity(emailIntent)
-            } catch (e: ActivityNotFoundException) {
-                showWarningDialog("Email")
+            customer.email?.let {
+                contactProviders.add(
+                    ContactProviderInfo(
+                        R.color.email_red,
+                        R.drawable.ic_email_white_24,
+                        it
+                    ) {
+                        try {
+                            ContactProvider.sendEmail(requireContext(), it)
+                        } catch (e: ActivityNotFoundException) {
+                            showWarningDialog("email")
+                        }
+                    }
+                )
+            }
+
+            customer.token?.let {
+                contactProviders.add(
+                    ContactProviderInfo(
+                        R.color.notification_cyan,
+                        R.drawable.ic_notifications_24,
+                        getString(R.string.send_notification)
+                    ) {
+                        navigateToSendNotification()
+                    }
+                )
             }
         }
+
+        return contactProviders.toList()
     }
+
+//    fun callCustomer() {
+//        val phone = Uri.parse("tel:${sharedViewModel.user.value!!.phone}")
+//        val phoneIntent = Intent(Intent.ACTION_DIAL, phone)
+//        phoneIntent.resolveActivity(requireContext().packageManager)?.let {
+//            try {
+//                startActivity(phoneIntent)
+//            } catch (e: ActivityNotFoundException) {
+//                showWarningDialog(getString(R.string.dial_app))
+//            }
+//
+//        }
+//    }
+//
+//    fun sendEmail() {
+//        val email = Uri.parse("mailto:${sharedViewModel.user.value!!.email}")
+//        val emailIntent = Intent(Intent.ACTION_SENDTO, email)
+//            .putExtra(Intent.EXTRA_TEXT, getString(R.string.email_signature))
+//        emailIntent.resolveActivity(requireContext().packageManager)?.let {
+//            try {
+//                startActivity(emailIntent)
+//            } catch (e: ActivityNotFoundException) {
+//                showWarningDialog("Email")
+//            }
+//        }
+//    }
 
     fun navigateToSendNotification() {
         val action =
--- app/src/main/java/com/homelab/appointmentadmin/utils/ContactProvider.kt
+++ app/src/main/java/com/homelab/appointmentadmin/utils/ContactProvider.kt
@@ -15,9 +15,12 @@ object ContactProvider {
     }
 
     fun sendEmail(context: Context, email: String) {
-        val emailUri = Uri.parse("mailto:$email")
-        val emailIntent = Intent(Intent.ACTION_SENDTO, emailUri)
-            .putExtra(Intent.EXTRA_TEXT, context.getString(R.string.email_signature))
+        val emailUri = Uri.parse("mailto:")
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = emailUri
+            putExtra(Intent.EXTRA_EMAIL, email)
+            putExtra(Intent.EXTRA_TEXT, context.getString(R.string.email_signature))
+        }
         emailIntent.resolveActivity(context.packageManager)?.let {
             context.startActivity(emailIntent)
         }
